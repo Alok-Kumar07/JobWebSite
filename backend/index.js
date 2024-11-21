059bdce58d0df6b1edc71bc5411fa92b64b2362a
Alok Kumar <92195912+Alok-Kumar07@users.noreply.github.com>
2024-11-21 15:00:21 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://firstjob-frontend.onrender.com'
+];
+
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:allowedOrigins,
     credentials:true
 }
 
